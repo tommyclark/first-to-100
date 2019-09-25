a712d6c1f5d5330bfa485902d42c9176016b7ea3
Tommy Clark <tommy.clark@sainsburys.co.uk>
2019-09-25 16:56:47 +0100

    Enable cors.

--- src/main/java/com/maddogs/game/configuration/SecurityConfig.java
+++ src/main/java/com/maddogs/game/configuration/SecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -34,9 +37,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/admin/*").authenticated()
                 .anyRequest().permitAll()
-                .and().csrf().disable()
+                .and()
+                .cors().and()
+                .csrf().disable()
                 .httpBasic();
     }
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
