d2f3846a64773fb6fcbd522106831ef0c14ec799
Tommy Clark <tommy.clark@sainsburys.co.uk>
2019-10-21 19:14:14 +0100

    Fix basic auth for admin console.

--- src/main/java/com/maddogs/game/configuration/SecurityConfig.java
+++ src/main/java/com/maddogs/game/configuration/SecurityConfig.java
@@ -35,8 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/admin/*").authenticated()
-                .anyRequest().permitAll()
+                .antMatchers("/admin*").authenticated()
                 .and()
                 .cors().and()
                 .csrf().disable()
